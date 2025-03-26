7796698f2b8e0f16217be59a510562ce4eb8e99d
Hun3431 <limdohun77@gmail.com>
2025-03-26 15:22:59 +0900

    [Feat] 404 페이지에 홈으로 돌아가기 버튼 추가 및 디자인 개선

--- src/pages/notfound/index.tsx
+++ src/pages/notfound/index.tsx
@@ -1,11 +1,31 @@
+import { useNavigate } from "react-router-dom";
+import { cn } from "cn-func";
+
 const NotFoundPage = () => {
+  const navigate = useNavigate();
+
+  const handleBackToHome = () => {
+    navigate("/");
+  };
   return (
-    <div className="w-full h-dvh flex flex-col items-center justify-center gap-10 text-black dark:text-white">
+    <div
+      className={cn(
+        "dark:bg-black dark:text-white",
+        "not-dark:bg-white not-dark:text-black",
+        "flex flex-col items-center justify-center gap-10",
+        "w-full h-dvh "
+      )}
+    >
       <div className="text-9xl font-semibold">404</div>
       <div className="text-3xl font-bold">Page Not Found</div>
-      <button className="bg-indigo-400 px-6 py-4 rounded-full text-white font-semibold">
-        Back to home
-      </button>
+      <div className="rounded-full bg-white">
+        <button
+          className="bg-indigo-400 px-6 py-4 rounded-full text-white font-semibold hover:opacity-80"
+          onClick={handleBackToHome}
+        >
+          Back to home
+        </button>
+      </div>
     </div>
   );
 };
