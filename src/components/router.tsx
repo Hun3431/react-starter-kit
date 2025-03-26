84757003d8d9496f777d3c43b979e125fc216a73
Hun3431 <limdohun77@gmail.com>
2025-03-26 14:47:08 +0900

    [Feat] 메인 페이지 및 404 페이지 컴포넌트로 분리

--- src/components/router.tsx
+++ src/components/router.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainPage from "../pages/main";
+import NotFoundPage from "../pages/notfound";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<>main</>} />
-        <Route path="*" element={<>NotFound</>} />
+        <Route path="/" element={<MainPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   );
