c48b566d45a8d70bafcfe1cfe6748e83f713031d
Hun3431 <limdohun77@gmail.com>
2025-03-26 15:05:10 +0900

    [Feat] 404 페이지 디자인 개선 및 홈으로 돌아가기 버튼 추가

--- src/pages/notfound/index.tsx
+++ src/pages/notfound/index.tsx
@@ -1,5 +1,13 @@
 const NotFoundPage = () => {
-  return <div>404 - Page Not Found</div>;
+  return (
+    <div className="w-full h-dvh flex flex-col items-center justify-center gap-10 text-black dark:text-white">
+      <div className="text-9xl font-semibold">404</div>
+      <div className="text-3xl font-bold">Page Not Found</div>
+      <button className="bg-indigo-400 px-6 py-4 rounded-full text-white font-semibold">
+        Back to home
+      </button>
+    </div>
+  );
 };
 
 export default NotFoundPage;
